e9b7cfe8b25fa479a950757f0832126df9e208e7
Diana Gomez <dianaegc094@gmail.com>
2021-09-04 22:20:59 -0400

    audio2

--- VideoGame/videogame.js
+++ VideoGame/videogame.js
@@ -1,31 +1,46 @@
 const $canvas =document.getElementById('canvas');
 const ctx=$canvas.getContext("2d")
+const width=canvas.style.width=window.innerWidth+'px';
+const heigth=canvas.style.height=window.innerHeight+'px';
+let gameInterval 
+let frames=0
 
-
-
-let img=new Image();
-img.src="/IronhackVideoGame/recursos/1399026.jpg"
-
-ctx.drawImage(img,0,0)
-img.onload=function(){
-    ctx.drawImage(img,0,0);
+//Class Background
+class Background{
+constructor(){
+    this.x=0;
+    this.y=0;
+    this.width=canvas.style.width=window.innerWidth+'px';
+    this.height=canvas.style.height=window.innerHeight+'px';
+    this.img=new Image();
+    this.img.src="/IronhackVideoGame/recursos/city.jpg"
+}
+//Metodo
+draw(){
+    if(this.y >= $canvas.height) this.y=0;
+    this.y++;
+    ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
+}
+}
+//Class Runners
+class Runner{
+    constructor(){
+        this.x=0;
+        this.y=0;
+        this.width=$canvas.width;
+        this.height=$canvas.height;
+        this.img=new Image();
+        this.img.src="/IronhackVideoGame/recursos/Man.png"
+}
 }
 
+//
 
-
-
-
-
-
-
-
-//console.log( "hola ")
-
-function fullScreen(){
-    canvas.style.width=window.innerWidth+'px';
-    canvas.style.height=window.innerHeight+'px';
-    console.log('Ancho de mi pantalla:' + window.innerWidth);
-    console.log('Alto de la pantalla:'+ window.innerHeight);
-
+//Instancias
+const board=new Background()
+//Motor del juego
+function updateGame(){
+    clearCanvas();
+    board.draw(); 
 }
-fullScreen();
+
