b9d0bb7aef60647d54f30f10f6c60f5586060ab6
Diana Gomez <dianaegc094@gmail.com>
2021-09-06 12:59:01 -0400

    Cambios 1

--- VideoGame/videogame.js
+++ VideoGame/videogame.js
@@ -1,46 +1,48 @@
-const $canvas =document.getElementById('canvas');
-const ctx=$canvas.getContext("2d")
-const width=canvas.style.width=window.innerWidth+'px';
-const heigth=canvas.style.height=window.innerHeight+'px';
-let gameInterval 
-let frames=0
+const $canvas=document.querySelector("canvas")
+const ctx=$canvas.getContext("2d");
+let getInterval;
+$canvas.width=canvas.width
+$canvas.height=canvas.height
+
 
 //Class Background
-class Background{
-constructor(){
-    this.x=0;
-    this.y=0;
-    this.width=canvas.style.width=window.innerWidth+'px';
-    this.height=canvas.style.height=window.innerHeight+'px';
-    this.img=new Image();
-    this.img.src="/IronhackVideoGame/recursos/city.jpg"
-}
-//Metodo
-draw(){
-    if(this.y >= $canvas.height) this.y=0;
-    this.y++;
-    ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
-}
-}
-//Class Runners
-class Runner{
+class Road{
     constructor(){
         this.x=0;
         this.y=0;
         this.width=$canvas.width;
         this.height=$canvas.height;
         this.img=new Image();
-        this.img.src="/IronhackVideoGame/recursos/Man.png"
+        this.img.src="/IronhackVideoGame/recursos/carretera.png"
+    }
+    //Método
+    draw(){
+        ctx.drawImage(this.img,this.x,this.y,this.width,this.height);
+    }
 }
+//Class Avatar
+class Avatar{
+    constructor(){
+        
+    }
 }
-
-//
-
 //Instancias
-const board=new Background()
-//Motor del juego
-function updateGame(){
-    clearCanvas();
-    board.draw(); 
-}
+const road=new Road();
 
+//event listeners 
+window.onload=()=>{
+    startGame();
+    background.draw();
+
+};
+
+//motor del juego
+function updateGame(){
+    road.draw()
+}
+function startGame(){
+    gameInterval=setInterval(updateGame,1000/60)
+}
+function clearCanvas(){
+    ctx.clearRect(0,0,$canvas.width,$canvas.height)
+}
